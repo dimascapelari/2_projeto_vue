bc32dffaa8265d0ba7b772c8cf42be659bc2a60e
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-11-26 15:57:47 -0300

    aula do projeto burger

---
+++ src/components/Picture.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img :src="avatar" :alt="descricao" />
+    <MudarImagem @mudar-imagem="trocarImagem" />
+  </div>
+</template>
+
+<script>
+import MudarImagem from "./MudarImagem.vue";
+
+export default {
+  components: {
+    MudarImagem,
+  },
+  name: "Picture",
+  data() {
+    return {
+      avatar: "/img/avatar.png",
+      descricao: "Dimas Capelari",
+    };
+  },
+  methods: {
+    trocarImagem() {
+      this.avatar = "/img/avatar2.png";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
